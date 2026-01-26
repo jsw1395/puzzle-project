751a34165b5090de7a583e4ce3feee15e0290529
jsw1395 <jsw1395@gmail.com>
2026-01-26 12:47:50 +0900

    add movement script for player

--- cave puzzle/Assets/Scripts/Player.cs
+++ cave puzzle/Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    float speed = 5.0f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +12,21 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(speed * Vector2.up * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(speed * Vector2.down * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(speed * Vector2.left * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(speed * Vector2.right * Time.deltaTime);
+        }
     }
 }
